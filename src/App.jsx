3ed7df3f51d938b84f6b024b5e97032b348997ae
Jessie Jalca <jessiejalca@gmail.com>
2023-01-29 01:23:14 -0500

    fix: Fetch data from a new API
    REASON
    The previous API didn't pull up enough data from a random
    Wikipedia page. This new one should be able to pull in much
    more to include a short description and hopefully an image.
    
    NOTE
    This Stack Overflow post helped clarify the issue & find
    the better API call:
    https://stackoverflow.com/questions/33614492/wikipedia-api-get-random-pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,28 @@ import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
-// wiki media api: list=random&rnlimit=1
-
 function App() {
   
   const [data, setData] = useState(null)
+
   useEffect(() => {
-    fetch(`https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&list=random&formatversion=2&rnnamespace=0`
-    )
+    fetch(`https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&generator=random&prop=revisions|images&rvprop=content&grnnamespace=0&grnlimit=1`)
       .then(response => response.json())
       .then(data => setData(data))
   }, [])
 
-  console.log(data)
-
-  if (data) 
+  if (data) {
+    const pages = data.query.pages
+    const pageData = pages[Object.keys(pages)[0]]
+    const pageTitle = pageData.title
+    console.log(pageData)
+    
     return (
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <div className="App">
+        <h1>{pageTitle}</h1>
+      </div>
     )
-
-  // const WIKI_TITLE = "Hackathon"
-  // const WIKI_DESCRIPTION = `A hackathon (also known as a hack day, hackfest, datathon or codefest; a portmanteau of hacking and marathon) is an event where people engage in rapid and collaborative engineering over a relatively short period of time such as 24 or 48 hours.`
-
-  // return (
-  //   <div className="App">
-  //     <h1>{WIKI_TITLE}</h1>
-  //     <p>{WIKI_DESCRIPTION}</p>
-  //   </div>
-  // )
+  }
 }
 
 export default App
